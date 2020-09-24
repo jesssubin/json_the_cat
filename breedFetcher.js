88efc779bc04022ac9b18eda66a90664501a7946
jesssubin <jess.s.moon@gmail.com>
2020-09-24 18:43:53 +0000

    test added

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,19 +1,23 @@
-const request = require('request');
-const breedName = process.argv[2];
 
-const urlAddress = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`;
-request(urlAddress, function(error, response, body) {
-
-  if (error) {
-    return console.log("URL is  not valid", error);
-  }
-  const data = JSON.parse(body);
-  const breed = data[0];
-  if (breed) {
-    console.log(breed.description);
-  } else {
-    console.log("Breed Not Found");
-    return error;
-  }
+const fetchBreedDescription = function(breedName, callback) {
   
-});
+  const request = require('request');
+  const urlAddress = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedName}`;
+
+  request(urlAddress, function(error, response, body) {
+
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      callback("Breed Not Found", null);
+    } else {
+      const breed = data[0].breeds[0];
+      console.log(breed);
+      callback(null, breed.description);
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
